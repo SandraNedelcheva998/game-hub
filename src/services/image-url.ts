d5e51c567ed7ced81383785c2828e0d4280c34c9
Sandra Nedelcheva <sandra_nedelceva@hotmail.com>
2025-10-03 20:57:29 +0200

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,8 @@
-const getCroppedImageUrl=(url:string)=>{
-    if(!url) return;
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp"
 
-    //TODO: fix error tuka zosto ne site igri imaat slika Uncaught TypeError: Cannot read properties of null (reading 'indexOf')
+const getCroppedImageUrl=(url:string)=>{
+    if(!url) return noImage;
+
     const target="media/"
     const index=url.indexOf(target)+target.length;
     return url.slice(0,index)+'crop/600/400/'+url.slice(index)
